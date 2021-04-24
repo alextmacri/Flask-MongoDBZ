a4b093c2dce21bc16c94846dd0e3b3a25b8db73f
Alex Macri <alexmacri04@gmail.com>
2021-04-24 01:47:07 -0400

    main.py imports and outline

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse, abort, fields, marshal_with
+from flask_mongoengine import MongoEngine
+
+
+# initializing and setting up Flask, API, and DB
+
+
+# defining the DBZ Character
+
+
+# defining the request parsers
+
+
+# defining the resource fields
+
+
+# defining the 'Character' resource
+
+
+# defining the 'Characters' resource
+
+
+# adding the resources to the API
+
+
+# running the Flask app
