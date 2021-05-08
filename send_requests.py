f5921977108df6da58f81efc67f2b374c3349812
Alex Macri <alexmacri04@gmail.com>
2021-05-08 11:51:30 -0400

    Updated request sender example file

--- send_requests.py
+++ send_requests.py
@@ -1,26 +1,25 @@
 import requests
 
-BASE = 'http://127.0.0.1:5000/'         # locally run Flask API
-
-
-# the (C)reate in CRUD
-resp = requests.post(BASE+'api/character/1', {'name': 'Goku'})
-print(resp.json())
-
-
-# the (R)ecieve in CRUD
-resp = requests.get(BASE+'api/character/1')
-print(resp.json())
-
-resp = requests.get(BASE+'api/characters')
-print(resp.json())
-
-
-# the (U)pdate in CRUD
-resp = requests.get(BASE+'api/character/1', {'name': 'Piccolo'})
-print(resp.json())
+BASE = 'http://127.0.0.1:5000/api/'             # locally run Flask API
 
 
 # the (D)elete in CRUD
-resp = requests.get(BASE+'api/character/1')
-print(resp)
+resp = requests.delete(BASE+'character/3')
+print('delete:          ', resp)
+
+# the (C)reate in CRUD
+resp = requests.post(BASE+'character/3', {'name': 'Goku', 'level': 18})
+print('post:            ', resp.json())
+
+
+# the (R)ecieve in CRUD
+resp = requests.get(BASE+'character/3')
+print('get (just #3):   ', resp.json())
+
+resp = requests.get(BASE+'characters')
+print('get (all):       ', resp.json())
+
+
+# the (U)pdate in CRUD
+resp = requests.put(BASE+'character/3', {'level': 20})
+print('put:             ', resp.json())
